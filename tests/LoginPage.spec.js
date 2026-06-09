7d7378835c78b0832fd3f076fd924ed5afc5ab71
Taiwo Odeniyi <taiwood_76@yahoo.co.uk>
2026-06-09 22:30:01 +0100

    my playwright javascript framework

---
+++ tests/LoginPage.spec.js
@@ -0,0 +1,18 @@
+import { test} from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage'
+//import { loginpage} from '../pages/LoginPage.js';
+
+test('Login to the application', async ({ page }) => { 
+    
+    const loginpage = new LoginPage(page);
+    await loginpage.gotologinPage();
+    await page.waitForTimeout(3000)
+    await loginpage.login('user1.n@yahoo.com', 'Oddy2026')
+    await page.waitForTimeout(3000)
+    await loginpage.verifyWelcomePage('user1.n@yahoo.com');
+
+      //const welcomeText = page.locator('#nameofuser');
+  //await expect(welcomeText).toContainText('Welcome user1.n@yahoo.com1');
+    
+
+})
