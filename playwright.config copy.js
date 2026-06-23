1c06086b18b6b1e7ba814fa2e685f049ec63cc8d
usertaiwo <116678620+usertaiwo@users.noreply.github.com>
2026-06-23 18:44:58 +0100

    jgjjg

--- playwright.config copy.js
+++ playwright.config copy.js
@@ -44,47 +44,31 @@ export default defineConfig({
 	/* Configure projects for major browsers */
 	projects: [
 		{
-			name: "chromium",
-			use: { ...devices["Desktop Chrome"] },
-		},
+			name: 'Safari',
+			use: {
+				broserName: 'webkit',
+				headless: false,
+				screenshot: 'on',
+				reporter: 'list',
+				
+				trace:'off'
 
-		// {
-		//    name: 'firefox',
-		//    use: { ...devices['Desktop Firefox'] },
-		// },
 
-		// {
-		//    name: 'webkit',
-		//    use: { ...devices['Desktop Safari'] },
-		// },
+			},
+			name: 'chrome',
+			use: {
+				broserName: 'chromium',
+				headless: true,
+				screenshot: 'only-on-failure',
+				reporter: 'list',
+				
+				trace:'on'
 
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
+			{
 
-		{
-			name: "Mobile Safari",
-			use: { ...devices["iPhone 16"] },
-		},
+			}
 
-		/* Test against branded browsers. */
-		{
-			name: "Microsoft Edge",
-			use: { ...devices["Desktop Edge"], channel: "msedge" },
-		},
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		}
 	],
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://localhost:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
 });
 
