747ad91c2012857e42d7eabd782a0f02311b31f8
usertaiwo <116678620+usertaiwo@users.noreply.github.com>
2026-06-23 14:21:07 +0100

    dfrgttgg

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -3,43 +3,38 @@ import {expect} from '@playwright/test'
 
     constructor(page) { 
         this.page = page;
-        this.loginLink = "//a[@id='login2']";
+        this.loginLink =page.locator( "//a[@id='login2']");
         //this.loginLink = page.getByRole('link', { name: 'Log in' })
-        this.userNameInput = '#loginusername';
-        this.passwordInput = '#loginpassword';
-        //this.passwordInput = 'a:has-text("#logInModal")'
-        this.loginButton = "//button[@onclick='logIn()']";
-        //this.welcomePage = page.getByRole("heading", { name: 'Welcome user1.n@yahoo.com' });
-        // this.welcomePage = page.locator("//a[@id='nameofuser']")
-        this.welcomePage = '#nameofuser'
-        this.ahdhhdhd = 'jfjfjfjfj'
-        // gttfdddss rr
-        //his.log.loginButton = "//button[@onclick='logIn()']"
-        //this.loginURL = 'https://www.demoblaze.com/index.html';
-        //https://www.demoblaze.com/index.html
-        //jdjdjdj
+        
+        this.userNameInput = page.locator("#loginusername");
+		this.passwordInput = page.locator("#loginpassword");
+		this.loginButton = page.locator("//button1[@onclick='logIn()']");
+				//this.welcomePage = page.getByRole("heading", { name: 'Welcome user1.n@yahoo.com' });
+				// this.welcomePage = page.locator("//a[@id='nameofuser']")
+		this.welcomePage = "#nameofuser";
+        
+        
+    
 
-        // NEW COMMENT
-        // newest comment
-        //hytg
+        
 
     }
 
     async gotologinPage() { 
         await this.page.goto('https://www.demoblaze.com/index.html');
+        
     }
     
  
      async login(username, password) {
        
-         //await this.page.locator(this.loginButton).click()
-         //await this.page.getByRole('button', { name: 'Log in' }).click();
-         //await this.loginLink.click();
-         await this.page.locator(this.loginLink).click();
-         await this.page.locator(this.userNameInput).fill(username);
-         await this.page.locator(this.passwordInput).fill(password);
+         
+         //await this.page.locator(this.loginLink).click();
+         await this.loginLink.click()
+         await this.userNameInput.fill(username);
+         await this.passwordInput.fill(password);
        
-         await this.page.locator(this.loginButton).click();
+         await this.loginButton.click();
 
      }
      
