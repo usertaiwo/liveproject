4b9e8bc4988bf00ff603029aae2bac82ff5da0d0
usertaiwo <116678620+usertaiwo@users.noreply.github.com>
2026-07-02 08:38:45 +0100

    github reporter added

--- playwright.config.js
+++ playwright.config.js
@@ -23,9 +23,10 @@ export default defineConfig({
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
+	reporter: "github",
 
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	// reporter: [['list'], ['json', {output:'test-results.json'}]],
+	// reporter: [['list'], ['json', {output:'test-results.json'}], ['github]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('')`. */
@@ -37,7 +38,7 @@ export default defineConfig({
 		colorScheme: "light",
 		video: "off",
 		screenshot: "only-on-failure",
-		reporter: "list","html","github",
+		reporter: "list",
 		locale: "es-ES",
 	},
 
